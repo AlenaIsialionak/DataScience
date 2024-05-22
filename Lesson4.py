a6d4020ea421bdc4a39e9ae92fc196530dee9a4e
AlenaIsialionak <alyonkaiselyonok@gmail.com>
2024-05-22 21:41:06 +0300

    change

--- Lesson4.py
+++ Lesson4.py
@@ -1,40 +1,62 @@
-def check(your: tuple, enemy: tuple) -> bool:
-    steps = [(-1, 2), (1, 2), (-1, -2), (1, -2)]
+def different_array1(size_array: int):
+    str1 = [[0 for _ in range(size_array)] for _ in range(size_array)]
 
-    x1, y1 = your
-    x2, y2 = enemy
+    for i in range(size_array):
+        for j in range(size_array):
+            if i < size_array//2 + 1:
+                if j >= size_array - i - 1:
+                    str1[i][j] = 1
+            else:
+                if j > i - 1:
+                    str1[i][j] = 1
+    for s in str1:
+        print(s)
 
-    for step in steps:
-        a, b = step
-        if 0 <= x1 + a <= 8 and x1 + a == x2 and 0 <= y1 + b <= 8 and y1 + b == y2:
-            return True
-    return False
+
+different_array1(5)
 
 
-# print(check((6, 6), (7, 5)))
-
-
-def different_array(size_array: int):
-    result = []
-
-    str1 = [['0'] * size_array for _ in range(size_array)]
-    print(str1)
+def different_array2(size_array: int):
+    str1 = [[0] * size_array for _ in range(size_array)]
 
     for i in range(size_array):
 
         for j in range(size_array):
             if i < size_array//2 + 1:
-                if j >= size_array - i - 1:
-                    str1[i][j] = "1"
+                if j <= i or j >= size_array - i - 1:
+                    str1[i][j] = 1
             else:
-                if j > i - 1:
-                    str1[i][j] = "1"
+                if j < size_array - i or j > i - 1:
+                    str1[i][j] = 1
     for s in str1:
         print(s)
 
-different_array(5)
+
+print("***************")
+different_array2(5)
 
 
+def different_array3(size_array: int):
+
+    str1 = [[0] * size_array for _ in range(size_array)]
+
+    for i in range(size_array):
+
+        for j in range(size_array//2 + 1):
+            if i < size_array//2 + 1:
+                if j >= i:
+                    str1[i][j] = 1
+                    str1[i][-j-1] = 1
+            else:
+                if j >= size_array - i - 1:
+                    str1[i][j] = 1
+                    str1[i][-j - 1] = 1
+    for s in str1:
+        print(s)
+
+
+print("****************")
+different_array3(5)
 
 
 
